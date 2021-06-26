66268ced7e8b547537d60d00718549c75e362f50
peter <ptmartinezsound@gmail.com>
2021-06-26 09:56:17 -0500

    installing service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,24 @@
+const FILE_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js"
+]
+
+const APP_PREFIX = "BudgetTracker-"
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILE_TO_CACHE)
+        })
+    )
+})
+
 console.log('Service worker registered!')
+
+self.addEventListener('install', function (e) {
+
+})
